bc9ecf4f2f7dd08037a7269d7d8acc34cd27a2eb
minibox24 <minibox724@gmail.com>
2021-09-02 21:51:01 +0900

    💄 Noto Sans KR 폰트 적용

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;900&display=swap");
+
 #app {
+  font-family: "Noto Sans KR", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
