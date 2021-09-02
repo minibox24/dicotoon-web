0ed97c0eef2f1c9d047dc846c11d9ad6ced60c7c
minibox24 <minibox724@gmail.com>
2021-09-02 23:39:29 +0900

    ✨ 헤더

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
+  <Header />
   <router-view />
 </template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  components: { Header },
+};
+</script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;900&display=swap");
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    <router-link class="url title" to="/">DicoToon</router-link>
+    <a class="url" href="#" target="_blank">봇 초대</a>
+    <a class="url" href="https://discord.gg/pbd2xXJ" target="_blank">
+      서포트 서버
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  padding-left: 0.5rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+.title {
+  font-weight: 900;
+  font-size: 1.5rem !important;
+}
+
+.url {
+  color: black;
+  text-decoration: none;
+  font-size: 1.2rem;
+}
+</style>
