34667f547df80c2267cce270644a56d4bffa42d5
minibox24 <minibox724@gmail.com>
2021-09-03 16:10:56 +0900

    ✨ about 탭

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -2,26 +2,49 @@
   <div class="menu">
     <div class="info">
       <span class="title">{{ name }}</span>
-      <div class="contributors">
-        <div
-          class="user-container"
-          v-for="(contributor, index) in contributors"
-          :key="index"
-          :src="contributor.avatar"
-        >
-          <img class="user-icon" :src="contributor.avatar" />
-          <div class="user-tooltip-container">
-            <div class="user-tooltip">
-              <img class="user-avatar" :src="contributor.avatar" />
-              <div class="user-info">
-                <span class="user-name">{{ contributor.name }}</span>
-                <span class="user-count">{{ contributor.count }}컷</span>
+      <div class="infos">
+        <div class="about">
+          <span class="subtitle">About</span>
+          <div class="about-items">
+            <div class="about-item">
+              <img src="@/assets/time.svg" />
+              <span>13일 전 시작</span>
+            </div>
+            <div class="about-item">
+              <img src="@/assets/images.svg" />
+              <span>653컷</span>
+            </div>
+          </div>
+        </div>
+        <div class="contributors-container">
+          <span class="subtitle">
+            Contributors
+            <span class="contributors-count">{{ contributors.length }}</span>
+          </span>
+          <div class="contributors">
+            <div
+              class="user-container"
+              v-for="(contributor, index) in contributors"
+              :key="index"
+              :src="contributor.avatar"
+            >
+              <img class="user-icon" :src="contributor.avatar" />
+              <div class="user-tooltip-container">
+                <div class="user-tooltip">
+                  <img class="user-avatar" :src="contributor.avatar" />
+                  <div class="user-info">
+                    <span class="user-name">{{ contributor.name }}</span>
+                    <span class="user-count">{{ contributor.count }}컷</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <hr />
+    <div class="tags"></div>
   </div>
 </template>
 
@@ -56,12 +79,48 @@ export default {
   padding: 2rem;
 }
 
+.info {
+  display: flex;
+}
+
 .title {
   font-size: 3rem;
   font-weight: 900;
 }
 
+.infos {
+  margin-left: auto;
+}
+
+.subtitle {
+  font-size: 1.2rem;
+}
+
+.about-items {
+  margin-top: 0.5rem;
+}
+
+.about-item {
+  display: flex;
+  align-items: center;
+  margin-top: 0.2rem;
+  gap: 0.2rem;
+}
+
+.contributors-container {
+  margin-top: 1rem;
+}
+
+.contributors-count {
+  font-size: 0.8rem;
+  background: #eff1f3;
+  padding: 0.2rem;
+  min-width: 20px;
+  border-radius: 2em;
+}
+
 .contributors {
+  margin-top: 0.5rem;
   width: 240px;
   display: flex;
   flex-wrap: wrap;
@@ -76,7 +135,7 @@ export default {
 .user-tooltip-container {
   position: relative;
   top: 10px;
-  left: -20px;
+  left: -240px;
 }
 
 .user-tooltip {
@@ -114,10 +173,14 @@ export default {
 
 .user-name {
   font-weight: 900;
-  font-size: 1.2rem;
+  font-size: 1.4rem;
 }
 
 .user-count {
   color: #4f5660;
+}
+
+hr {
+  border: 1px solid rgba(170, 170, 170, 0.34);
 }
 </style>
