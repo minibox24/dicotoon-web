0f48e219741bdd138692d06c4f4544ffa0b1f26b
minibox24 <minibox724@gmail.com>
2021-09-03 11:05:49 +0900

    ➕ vue-toast-notification

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueToast from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-default.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(VueToast).mount("#app");
